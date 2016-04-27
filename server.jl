464791c45c91802b31c16b16f540eeee7eedd6dc
Hiroyuki Shindo <hshindo@outlook.com>
2016-04-27 17:35:02 +0900

    Fix server

--- server.jl
+++ server.jl
@@ -10,10 +10,9 @@ using Merlin
 include("conf.jl")
 
 const clients = Dict()
-const path = dirname(@__FILE__)
 const tokenizer = load("models/tokenizer_50.jld", "tokenizer")
 const conf = begin
-  e = readconf(joinpath(path,"conf/visual.conf"))
+  e = readconf(joinpath(dirname(@__FILE__),"conf/visual.conf"))
   d = Dict("entity_types" => e)
   JSON.json(d)
 end
@@ -21,7 +20,7 @@ end
 const postagger = begin
   println("training postagger...")
   p = POSTagger()
-  POSTagging.train(p, "$(path)/models/wsj_00-18.conll", "$(path)/models/wsj_22-24.conll")
+  POSTagging.train(p, "models/wsj_00-18.conll", "models/wsj_22-24.conll")
   println("finish.")
   p
 end
